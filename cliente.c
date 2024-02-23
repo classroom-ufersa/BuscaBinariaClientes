81338d84f62ab8ddb3f97db8b85b9a1a6fa4e8ad
Difierro <gustavo7867grdr@gmail.com>
2024-02-23 11:19:39 -0300

    padronizando nome de variáveis

--- cliente.c
+++ cliente.c
@@ -64,20 +64,20 @@ void mostrarDados(struct cliente *Cliente, int qtdClientes)
     }
 };
 
-int comparaID(const void * pa, const void * pb){
-    const struct cliente * p1 = pa;
-    const struct cliente * p2 = pb;
-    return p1->id - p2->id;
+int comparaID(const void * pointerA, const void * pointerB){
+    const struct cliente * pointer1 = pointerA;
+    const struct cliente * pointer2 = pointerB;
+    return pointer1->id - pointer2->id;
 }
 
 void ordenaId(struct cliente *Cliente, int qtdClientes){
     qsort(Cliente, qtdClientes, sizeof(struct cliente), comparaID);
 }
 
-int comparaNome(const void * pa, const void * pb){
-    const struct cliente * p1 = pa;
-    const struct cliente * p2 = pb;
-    return strcmp(p1->nome, p2->nome);
+int comparaNome(const void * pointerA, const void * pointerB){
+    const struct cliente * pointer1 = pointerA;
+    const struct cliente * pointer2 = pointerB;
+    return strcmp(pointer1->nome, pointer2->nome);
 }
 
 void ordenaNome(struct cliente *Cliente, int qtdClientes){
@@ -87,32 +87,31 @@ void ordenaNome(struct cliente *Cliente, int qtdClientes){
 // Função para buscar o cliente pelo ID (Busca Binária por ID).
 void buscaBinaria_id(struct cliente *clientes, int qtdClientes){
     ordenaId(clientes, qtdClientes);
-    int Numero_desejado;
-    char continuar;
+    int numeroDesejado = 0;
     int encontrado = 0; 
-    printf("Informe o algarismo correspondente ao ID caro senhor(a):\n");
-    scanf("%d", &Numero_desejado);
-    int MAX_posicion = qtdClientes - 1; // Define a maior posição de (0 - 49) = 50 - 1 = 49.
-    int MIN_posicion = 0; // Define a menor posição que o número deve estar.
-    int Middle_posicion = 0; // A posição média será max + min/ 2 quebrando ao meio.
+    printf("Informe o Id do cliente: \n");
+    scanf("%d", &numeroDesejado);
+    int max = qtdClientes - 1; // Define a maior posição de (0 - 49) = 50 - 1 = 49.
+    int min = 0; // Define a menor posição que o número deve estar.
+    int meio = 0; // A posição média será max + min/ 2 quebrando ao meio.
 
-    while(MIN_posicion <= MAX_posicion){
+    while(min <= max){
         // Middle será a posição do meio no caso sempre será o resultado dessa conta.
-        Middle_posicion = ((MAX_posicion + MIN_posicion)/2); 
+        meio = ((max + min)/2); 
 
-        if(clientes[Middle_posicion].id == Numero_desejado){ 
+        if(clientes[meio].id == numeroDesejado){ 
             // Quando o middle for o numero desejado ele imprime os dados.
-            printf("Nome: %s\n", clientes[Middle_posicion].nome);
-            printf("Endereco: %s\n", clientes[Middle_posicion].endereco);
-            printf("ID: %d\n", clientes[Middle_posicion].id);
+            printf("Nome: %s\n", clientes[meio].nome);
+            printf("Endereco: %s\n", clientes[meio].endereco);
+            printf("ID: %d\n", clientes[meio].id);
             encontrado = 1; // se o id e seus dados correspondentes forem encontrado.
             break;
         }else{
-            if (clientes[Middle_posicion].id > Numero_desejado){ 
-                MAX_posicion = Middle_posicion - 1; 
+            if (clientes[meio].id > numeroDesejado){ 
+                max = meio - 1; 
                 //  Se a middle for maior que o número desejado, a direita é deletada.
             }else{
-                MIN_posicion = Middle_posicion + 1;
+                min = meio + 1;
                 //  Se o Middle_posicion for menor que o número desejado, .a esquerda é deletada. 
             }
         }
