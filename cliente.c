c752261033c1566317596775d467c72f0669bbef
Difierro <gustavo7867grdr@gmail.com>
2024-02-26 12:13:43 -0300

    removendo comentarios e revisando código

--- cliente.c
+++ cliente.c
@@ -6,63 +6,47 @@ struct cliente
     char endereco[70];
     int id;
 };
-// Como utilizo essa função para contar e alocar, preciso receber o array, então:
-struct cliente *contaClientes(int *qtdC)
-{
-    // Declarando variáveis:
+
+struct cliente *contaClientes(int *qtdC){
     int tamanho = 0;
     char linha[101];
     FILE *arquivo;
 
     arquivo = fopen("DataBase.txt", "r");
 
-    // No arquivo de texto, uma linha representa um cliente. Então contamos:
-    while (fscanf(arquivo, "%[^\n]\n", linha) != EOF)
-    {
+    while (fscanf(arquivo, "%[^\n]\n", linha) != EOF){
         tamanho++;
     }
 
-    // Fechando...
     fclose(arquivo);
 
-    // Aloca memória para o número de clientes
     struct cliente *clientes = malloc(tamanho * sizeof(struct cliente));
 
-    // Retorna o número de clientes através do ponteiro qtdC
     *qtdC = tamanho;
 
-    // Retorna o array para que possa ser utilizado em outros lugares do código:
     return clientes;
 }
 
-// Função para leitura e coleta dos dados:
-void coletaDados(struct cliente *clientes, int qtdC)
-{
-    // Declarando as variáveis:
+void coletaDados(struct cliente *clientes, int qtdC){
     int i = 0;
 
     FILE *dados;
     dados = fopen("DataBase.txt", "r");
 
-    // Criando um loop até o fim do arquivo...
-    while (i < qtdC)
-    {
+    while (i < qtdC){
         fscanf(dados, " %[^\t]\t %[^\t]\t%d", clientes[i].nome, clientes[i].endereco, &clientes[i].id);
         i++;
     }
 
-    // Fechando...
     fclose(dados);
 }
-// Função para mostrar dados (apenas para fins de verificação):
-void mostrarDados(struct cliente *Cliente, int qtdClientes)
-{
+
+void mostrarDados(struct cliente *Cliente, int qtdClientes){
     int j;
-    for (j = 0; j < qtdClientes; j++)
-    {
+    for (j = 0; j < qtdClientes; j++){
         printf("Nome: %s, Endereço: %s, ID: %d\n", Cliente[j].nome, Cliente[j].endereco, Cliente[j].id);
     }
-};
+}
 
 int comparaID(const void * pointerA, const void * pointerB){
     const struct cliente * pointer1 = pointerA;
@@ -91,34 +75,29 @@ void buscaBinariaId(struct cliente *clientes, int qtdClientes){
     int encontrado = 0; 
     printf("Informe o Id do cliente: \n");
     scanf(" %d", &numeroDesejado);
-    int max = qtdClientes - 1; // Define a maior posição de (0 - 49) = 50 - 1 = 49.
-    int min = 0; // Define a menor posição que o número deve estar.
-    int meio = 0; // A posição média será max + min/ 2 quebrando ao meio.
+    int max = qtdClientes - 1; 
+    int min = 0; 
+    int meio = 0;
 
     clock_t inicio = clock();
     while(min <= max){
-        // Middle será a posição do meio no caso sempre será o resultado dessa conta.
         meio = ((max + min)/2); 
 
         if(clientes[meio].id == numeroDesejado){ 
-            // Quando o middle for o numero desejado ele imprime os dados.
             printf("Nome: %s\n", clientes[meio].nome);
             printf("Endereco: %s\n", clientes[meio].endereco);
             printf("ID: %d\n", clientes[meio].id);
-            encontrado = 1; // se o id e seus dados correspondentes forem encontrado.
+            encontrado = 1;
             break;
         }else{
             if (clientes[meio].id > numeroDesejado){ 
                 max = meio - 1; 
-                //  Se a middle for maior que o número desejado, a direita é deletada.
             }else{
                 min = meio + 1;
-                //  Se o Middle_posicion for menor que o número desejado, .a esquerda é deletada. 
             }
         }
     }
     if (!encontrado) {
-        // Se o id e seus dados correspondentes não forem encontrado.
         printf("ID nao encontrado.\n");
     } 
 
@@ -129,19 +108,19 @@ void buscaBinariaId(struct cliente *clientes, int qtdClientes){
 void buscaBinariaNome(struct cliente *clientes, int qtdClientes)
 {
     ordenaNome(clientes, qtdClientes);
-    char nomeDesejado[50]; //vai armazenar o nome que o usário deseja buscar
+    char nomeDesejado[50];
     int encontrado = 0; 
-    int min = 0; //parte iniical do vetor
-    int max = qtdClientes - 1; //fim do vetor
+    int min = 0; 
+    int max = qtdClientes - 1;
     int meio = 0;
     printf("Digite o nome desejado: \n");
-    scanf(" %[^\n]", nomeDesejado); //está recebendo o nome que deve ser buscado
+    scanf(" %[^\n]", nomeDesejado);
 
     clock_t inicio = clock();
-    while(min <= max ){ //enquanto o valor inicial do vetor for menor ou igual ao final, vai realizar a operação até que se encontre o nome desejadoo
+    while(min <= max ){
         meio = (min+max)/2;
         
-        int comparacao = strcmp(clientes[meio].nome, nomeDesejado); //realiza a comparação das strings
+        int comparacao = strcmp(clientes[meio].nome, nomeDesejado);
             if(comparacao == 0){
                 printf("================================\n");
                 printf("Nome: %s\n", clientes[meio].nome);
@@ -152,12 +131,11 @@ void buscaBinariaNome(struct cliente *clientes, int qtdClientes)
             } 
             else if(comparacao > 0) { 
                 max = meio -1;
-           }else{ //se o nome buscado é menor que o do meio do vetor
-                min = meio + 1; //atualiza o índice para a parte que é superior do vetor
+           }else{
+                min = meio + 1;
             }
     }
     if (!encontrado) {
-        // Se o id e seus dados correspondentes não forem encontrado.
         printf("Cliente nao encontrado.\n");
     }
 
